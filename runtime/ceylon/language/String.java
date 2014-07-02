73ecc8f06cf81c06d1eda166e6a27925fca485d5
gavinking <gavin@ceylon-lang.org>
2014-07-02 16:57:52 +0200

    rename sequences() to partition()

--- runtime/ceylon/language/String.java
+++ runtime/ceylon/language/String.java
@@ -1856,8 +1856,8 @@ public final class String
     }
     
     @Ignore
-    public static Iterable<? extends Sequential<? extends Character>,?> sequences(java.lang.String value, long length) {
-    	return instance(value).sequences(length);
+    public static Iterable<? extends Sequential<? extends Character>,?> partition(java.lang.String value, long length) {
+    	return instance(value).partition(length);
     }
     
 }
